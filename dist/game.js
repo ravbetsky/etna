b4db50110fe2431ae7cfb972a79edd20c15d6fed
Artem Ravbetski <ravbetsky@gmail.com>
2017-04-22 15:05:00 +0000

    adaptive screen size ready

--- dist/game.js
+++ dist/game.js
@@ -4,37 +4,54 @@ var Game = function() {
 /* global THREE */
 Game.prototype.start = function() {
   // Создаем сцену
-  var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  var self = this;
+  this.scene = new THREE.Scene();
+  this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+  
+   // Создаем канвас для WebGL, добавляем к документу и создаем рендерер
+  this.renderer = new THREE.WebGLRenderer();
+  this.requestResize()
+  document.body.appendChild(this.renderer.domElement);
+  
+  window.onresize = this.requestResize.bind(this);
 
-  // Создаем канвас для WebGL, добавляем к документу и создаем рендерер
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild(renderer.domElement);
-
-  // Создаем зелены кубик
-  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  var cube = new THREE.Mesh( geometry, material );
-
-  // Добавляем кубик на сцену
-  scene.add(cube);
-
-  // смещаем камеру чтобы увидеть кубик
-  camera.position.z = 6;
-
+  this.init()
+  
   render()
 
   function render() {
-
-    cube.rotation.y += 0.01
-    cube.rotation.z += 0.01
-
-    requestAnimationFrame(render);
-	  renderer.render(scene, camera);
+    self.gameLoop();
+	  self.renderer.render(self.scene, self.camera);
+	  
+	  requestAnimationFrame(render);
   }
+}
+
+Game.prototype.init = function() {
+  // Создаем зелены кубик
+  var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  this.cube = new THREE.Mesh( geometry, material );
+
+  // Добавляем кубик на сцену
+  this.scene.add(this.cube);
+
+  // Cмещаем камеру чтобы увидеть кубик
+  this.camera.position.z = 6; 
+}
+
+Game.prototype.requestResize = function() {
+  this.camera.aspect = window.innerWidth / window.innerHeight;
+  this.camera.updateProjectionMatrix();
+  this.renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+Game.prototype.gameLoop = function(scene, camera) {
+  this.cube.rotation.x += 0.05
+  this.cube.rotation.z += 0.05
 }
 
 var game = new Game()
 
 game.start()
+
